c1f270da02ed2d45e19ec1c251db175346e12e23
Vladislav B <bogdanovichu@mail.ru>
2022-08-21 15:40:12 +0700

    Added MainPerson.java and Person.java in ru.person.java package

---
+++ Work_№4 (Java)/src/ru/person/java/MainPerson.java
@@ -0,0 +1,26 @@
+package ru.person.java;
+
+import java.util.Scanner;
+
+public class MainPerson {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите имя:" );
+        String name = scanner.nextLine();
+
+        System.out.println("Введите отчество:" );
+        String middleName = scanner.nextLine();
+
+        System.out.println("Введите фамилию:" );
+        String familyName = scanner.nextLine();
+
+        System.out.println("Введите возраст:" );
+        int age = scanner.nextInt();
+
+        Person person = new Person(name, middleName, familyName, age);
+
+        System.out.println("Год рождения: " + person.getBirthYear());
+
+        System.out.println("Объект класса Person был создан: " + person);
+    }
+}
---
+++ Work_№4 (Java)/src/ru/person/java/Person.java
@@ -0,0 +1,71 @@
+/*•Создать класс Person
+•В нём: объявить три строковых поля name, middleName, familyName
+•Описать конструктор, при помощи которого заполняются поля
+•Реализовать геттеры и сеттеры для полей
+•Добавить метод toString
+•После этого написать небольшую программу с использованием этого класса
+•В класс Person добавить поле age
+•Реализовать геттер и сеттер для поля age
+•Сделать метод для получения года рождения человека
+•В классе Main создать объект класса Person, установить ему значение поля age и вывести на экран год рождения человека*/
+
+package ru.person.java;
+
+import java.time.LocalDate;
+
+public class Person {
+    private String name;
+    private String middleName;
+    private String familyName;
+    private int age;
+
+    public Person (String name, String middleName, String familyName, int age) {
+        this.name = name;
+        this.middleName = middleName;
+        this.familyName = familyName;
+        this.age = age;
+    }
+
+    public String getName () {
+        return name;
+    }
+
+    public String getMiddleName () {
+        return middleName;
+    }
+
+    public String getFamilyName () {
+        return familyName;
+    }
+
+    public int getAge () {
+        return age;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public void setMiddleName (String middleName) {
+        this.middleName = middleName;
+    }
+
+    public void setFamilyName (String familyName) {
+        this.familyName = familyName;
+    }
+
+    public void setAge (int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + familyName + " " + name + " " + middleName + ". Возраст - " + age + "]";
+    }
+
+    public int getBirthYear() {
+        LocalDate now = LocalDate.now();
+        int year = now.getYear();
+        return year - age;
+    }
+}
